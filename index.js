b8c6ac021d8c0b06c91eeb20d1f1e0d67eedbcab
Thim Lohse <thim.lohse@netlight.com>
2019-09-12 14:05:09 +0200

    make it better

--- index.js
+++ index.js
@@ -11,3 +11,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`App is listening on port: ${PORT}`);
 });
+
+//"build": "npm run clean && babel -d ./dist ./ -s --ignore 'node_modules'",
