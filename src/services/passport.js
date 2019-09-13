0918c64a85981bec5d08d69b3226a2b4b1e41782
Thim Lohse <thim.lohse@netlight.com>
2019-09-13 16:43:52 +0200

    added proxy routing

--- src/services/passport.js
+++ src/services/passport.js
@@ -23,7 +23,8 @@ passport.use(
     {
       clientID: googleClientID,
       clientSecret: googleClientSecret,
-      callbackURL: 'http://localhost:5000/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     // Obtain the user information and save it to a database
     (accessToken, refreshToken, profile, done) => {
