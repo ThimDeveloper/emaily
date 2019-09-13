7010338856cdcaedde27c29801ff6a712c9251d2
Thim Lohse <thim.lohse@netlight.com>
2019-09-13 16:37:33 +0200

    keys setup

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,10 +1,3 @@
-// PROD OOrRJAtTSL1a9Bfr
-
-// PROD URI: mongodb+srv://emailyProd:OrRJAtTSL1a9Bfr@cluster0-2clps.mongodb.net/test?retryWrites=true&w=majority
-
-// PROD gUser: 1003344373059-vo279ceibph5ufosg67bdciemvacrkft.apps.googleusercontent.com
-// PROD gSec: kADEDcHb1hey3CApowOf7M58
-
 if (process.env.NODE_ENV === 'production') {
   // in production environment, return production keys
   module.exports = require('./prod');
--- src/config/prod.js
+++ src/config/prod.js
@@ -11,3 +11,10 @@ module.exports = {
   mongoURI,
   cookieKey
 };
+
+// PROD OOrRJAtTSL1a9Bfr
+
+// PROD URI: mongodb+srv://emailyProd:OrRJAtTSL1a9Bfr@cluster0-2clps.mongodb.net/test?retryWrites=true&w=majority
+
+// PROD gUser: 1003344373059-vo279ceibph5ufosg67bdciemvacrkft.apps.googleusercontent.com
+// PROD gSec: kADEDcHb1hey3CApowOf7M58
