2c55f57f23e1c16aaaecf10e8a0d33397331ee38
Thim Lohse <thim.lohse@netlight.com>
2019-09-21 23:33:46 +0200

    Added wildcard to take care of unknown routes

--- src/routes/withProductionClient.js
+++ src/routes/withProductionClient.js
@@ -5,9 +5,19 @@ module.exports = app => {
   if (process.env.NODE_ENV === 'production') {
     console.log('Using production build of client and server.');
     app.use(express.static(path.join(__dirname, '../../client/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(
+        path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html')
+      );
+    });
   } else if (process.env.NODE_ENV === 'test') {
     console.log('Testing production build of client and server.');
     app.use(express.static(path.join(__dirname, '../../client/build')));
+    app.get('*', (req, res) => {
+      res.sendFile(
+        path.resolve(__dirname, '..', '..', 'client', 'build', 'index.html')
+      );
+    });
   } else {
     console.log('Using development client and server.');
   }
